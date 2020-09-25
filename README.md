9d37943a56be3ec2524d74af0f61bbe564cdb699
uocmedia <toronto@uocmedia.com>
2020-09-24 20:52:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-77ezxo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-77ezxo)
---
+++ index.js
@@ -0,0 +1,40 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = convertStringintoLeet("Let's have some fun.");
+
+function convertStringintoLeet (source)
+{
+    let sourceArray = source.split();
+    return sourceArray.reduce((total, amount) => {
+        switch (amount) {
+            case 'a':
+                total.push('4');
+                break;
+            case 'e':
+                total.push('3');
+                break;
+            case 'i':
+                total.push('1');
+                break;
+            case 'o':
+                total.push('0');
+                break;
+            case 's':
+                total.push('5');
+                break;
+            case 't':
+                total.push('7');
+                break;
+            case 'b':
+                total.push('5');
+                break;
+            default:
+                total.push(amount)
+        }
+        return total;
+    }, []).join();
+}
+
